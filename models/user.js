12da95d45f5b16b9678be8e30020cc518475822d
Girish keleng <girishkeleng30@gmail.com>
2024-09-12 11:42:27 +0530

    Refactor user model and migration to remove unnecessary userType options

--- models/user.js
+++ models/user.js
@@ -1,25 +1,99 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
-module.exports = (sequelize, DataTypes) => {
-  class User extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
+
+const { DataTypes } = require("sequelize");
+const sequelize = require("../config/databaseConfig");
+const { up } = require("../migrations/20240911163034-create-user");
+
+ 
+
+const user = sequelize.define('user',{
+  id:{
+    allowNull:false,
+    autoIncrement:true,
+    primaryKey:true,
+    type:DataTypes.INTEGER
+  },
+  userType:{
+    type:DataTypes.ENUM('0','1'),
+    allowNull:false,
+    validate:{
+      notNull:{
+        msg:'Please enter a valid user type'
+      },
+      notEmpty:{
+        msg:"UserType cannot be empty"
+      },
     }
-  }
-  User.init({
-    firstName: DataTypes.STRING,
-    lastName: DataTypes.STRING,
-    email: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'user',
-  });
-  return User;
-};
+  },
+  firstName:{
+    type:DataTypes.STRING,
+    allowNull:false,
+    validate:{
+      notNull:{
+        msg:'Please enter a valid first name'
+      },
+      notEmpty:{
+        msg:"First name cannot be empty"
+      },
+    }
+  },
+  lastName:{
+    type:DataTypes.STRING,
+    allowNull:false,
+    validate:{
+      notNull:{
+        msg:"Please enter a valid last name"
+      },
+      notEmpty:{
+        msg:"Last name cannot be empty"
+      },
+    }
+  },
+  email:{
+    type:DataTypes.STRING,
+    allowNull:false,
+    validate:{
+      notNull:{
+        msg:"Please enter a valid email"
+      },
+      notEmpty:{
+        msg:"Email cannot be empty"
+      },
+      }
+  },
+  password:{
+    type:DataTypes.STRING,
+    allowNull:false,
+    validate:{
+      notNull:{
+        msg:"Please enter a valid password"
+      },
+      notEmpty:{
+        msg:"Password cannot be empty"
+      },
+    }
+  },
+  // confirmPassword:{
+  //   type:DataTypes.VIRTUAL,
+    
+  // }
+  createdAt:{
+    allowNull:false,
+    type:DataTypes.DATE
+  },
+  updatedAt:{
+    allowNull:false,
+    type:DataTypes.DATE
+  },
+   deletedAt:{
+    type:DataTypes.DATE
+   }
+
+},{
+  paranoid:true,
+  freezeTableName:true,
+  modelName:'user',
+})
+
+
+module.exports = user;
