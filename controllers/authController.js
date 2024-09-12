418569a51e2aa1447090bc1f2500b0ae9bdd5f0c
Girish keleng <girishkeleng30@gmail.com>
2024-09-12 12:44:18 +0530

    Define the authController

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,5 @@
-const jwt = require("jsonwebtoken")
+const jwt = require("jsonwebtoken");
+const user = require("../models/user");
 
 
 const generateToken =(payload)=>{
@@ -6,3 +7,30 @@ const generateToken =(payload)=>{
         expiresIn:process.env.JWT_EXPIRES_IN,
     })
 };
+
+const signup = async(req,res,next)=>{
+    const body = req.body;
+
+    if(!['1','2'].includes(body.userType)){
+        throw new Error("Please enter a valid user type",400)
+    }
+    const newUser = await user.create({
+        userType:body.userType,
+        firstName:body.firstName,
+        lastName:body.lastName,
+        email:body.email,
+        password:body.password,
+    }
+
+ );
+ if(!newUser){
+    return next(new Error("User not created",400));
+ }
+
+ const result = newUser.toJSON();
+ delete result.password;
+ delete result.deletedAt;
+ result.token= generateToken({
+    id:result.id
+ })
+}
