8b206efd395cef1954ef6b1e9b2d8f41660b5a15
Girish keleng <girishkeleng30@gmail.com>
2024-09-16 15:57:31 +0530

    Refactor countryController.js and countryRoute.js, update etByQuery route parameter

--- controllers/countryController.js
+++ controllers/countryController.js
@@ -1,81 +1,76 @@
 const catchAsync = require('../utils/catchError');
-const {user} = require('../models');
-const {country} = require('../models');
+const { user } = require('../models');
+const { country } = require('../models');
 const AppError = require("../utils/appError");
- 
+const { where } = require('sequelize');
 
 
 
-const createCountry = catchAsync(async(req,res,next)=>{
-    const body = req.body;
-     const userId = req.user.id;
-     console.log(req.user);
-    const userType = req.user.userType;
-      
-    if (userType !== '0') {
-        return next(new AppError('You are not authorized to create a country', 403));
-    }
 
-     const newCountry = await country.create({
-        name:body.name,
-        iso_code:body.iso_code,
-        capital:body.capital,
-        countryImage:body.countryImage,
-        coastline_km:body.coastline_km,
-        climate:body.climate,
-        createdBy:userId,
-     });
+const createCountry = catchAsync(async (req, res, next) => {
+   const body = req.body;
+   const userId = req.user.id;
+   console.log(req.user);
+   const userType = req.user.userType;
 
-     return res.status(201).json({
-        status: 'success',
-        data:newCountry
-     });
+   if (userType !== '0') {
+      return next(new AppError('You are not authorized to create a country', 403));
+   }
+
+   const newCountry = await country.create({
+      name: body.name,
+      iso_code: body.iso_code,
+      capital: body.capital,
+      countryImage: body.countryImage,
+      coastline_km: body.coastline_km,
+      climate: body.climate,
+      createdBy: userId,
+   });
+
+   return res.status(201).json({
+      status: 'success',
+      data: newCountry
+   });
 
 });
 
 
-const getAllCountry = catchAsync(async(req,res,next)=>{
+const getAllCountry = catchAsync(async (req, res, next) => {
    const result = await country.findAll();
 
    res.status(200).json({
-      message:"success",
-      data:result
+      message: "success",
+      data: result
    })
 
 
 });
 
 
-const getByIso = catchAsync(async(req,res,next)=>{
-   const iso = req.params.iso;
-   console.log(iso);
-   const result = await country.findOne({where:{iso_code:iso}});
 
-   if(!result){
-      return next(new AppError('No country found',404))
+const getByQuery = catchAsync(async (req, res, next) => {
+   const { name, iso } = req.query;
+ 
+   let result;
+   if (name) {
+     result = await country.findOne({ where: { name: name } });
+     if (!result) {
+       return next(new AppError('No country found with name ' + name, 404));
+     }
+   } else if (iso) {
+     result = await country.findOne({ where: { iso_code: iso } });
+     if (!result) {
+       return next(new AppError('No country found with iso code ' + iso, 404));
+     }
+   } else {
+     return next(new AppError('Please provide a name or iso query', 400));
    }
-
+ 
    res.status(200).json({
-      message:'success',
-      data:result
+     message: 'success',
+     data: result,
    });
-});
+ });
 
 
-const getByName = catchAsync(async(req,res,next)=>{
-   const name = req.params.name;
-   const result = await country.findOne({where:{name:name}});
-   
-   if(!result){
-      return next(new AppError('No country found with name'+ name, 404));
-   }
-   
-   res.status(200).json({
-      message:'success',
-      data:result,
-   })
-
-})
-
-
-module.exports = {createCountry, getAllCountry, getByIso,  getByName };
+module.exports = { createCountry, getAllCountry, getByQuery };
--- routes/countryRoute.js
+++ routes/countryRoute.js
@@ -1,16 +1,20 @@
 const express = require('express');
-const {createCountry, getAllCountry, getByIso, getByName } = require('../controllers/countryController');
+const {createCountry, getAllCountry,  getByQuery} = require('../controllers/countryController');
 const {authentication} = require('../controllers/authController');
 const router = express.Router();
 
 router.route('/')
-.post(authentication, createCountry);
+.post(authentication, createCountry)
+.get(authentication, getByQuery)
+ 
+
+
 router.route('/getAll')
 .get(authentication,getAllCountry);
 
 
-router.route('/:iso').get(authentication,getByIso);
+ 
 
-router.route('/:name').get(authentication,getByName);
+ 
 
 module.exports = router;
