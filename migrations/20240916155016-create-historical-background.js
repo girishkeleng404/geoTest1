2e0cae55fae6993f96fda5a2222d7388bea09033
Girish keleng <girishkeleng30@gmail.com>
2024-09-16 21:22:44 +0530

    Add migration and model for historical background

---
+++ migrations/20240916155016-create-historical-background.js
@@ -0,0 +1,35 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('historical_backgrounds', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      country_id:{
+        type:Sequelize.INTEGER,
+        references:{
+          model: 'country',
+          key: 'id',
+        }
+      },
+      background_description: {
+        type: Sequelize.TEXT
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('historical_backgrounds');
+  }
+};
