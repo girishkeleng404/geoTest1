5d5a9be97939b3c113877d0d8085673edc226474
Girish keleng <girishkeleng30@gmail.com>
2024-09-17 15:40:46 +0530

    Refactor countryController.js and countryRoute.js, update getByQuery route parameter

--- controllers/countryController.js
+++ controllers/countryController.js
@@ -8,56 +8,66 @@ const { where } = require('sequelize');
 
 
 const createCountry = catchAsync(async (req, res, next) => {
-   const body = req.body;
-   const userId = req.user.id;
-   console.log(req.user);
-   const userType = req.user.userType;
+  const body = req.body;
+  const userId = req.user.id;
+  console.log(req.user);
+  const userType = req.user.userType;
 
-   if (userType !== '0') {
-      return next(new AppError('You are not authorized to create a country', 403));
-   }
+  if (userType !== '0') {
+    return next(new AppError('You are not authorized to create a country', 403));
+  }
 
-   const newCountry = await country.create({
-      name: body.name,
-      iso_code: body.iso_code,
-      capital: body.capital,
-      countryImage: body.countryImage,
-      coastline_km: body.coastline_km,
-      climate: body.climate,
-      createdBy: userId,
-   });
+  const newCountry = await country.create({
+    name: body.name,
+    iso_code: body.iso_code,
+    capital: body.capital,
+    countryImage: body.countryImage,
+    coastline_km: body.coastline_km,
+    climate: body.climate,
+    createdBy: userId,
+  });
 
-   if(body.background_description){
-     await historical_bg.create({
-      country_id:newCountry.id,
-      background_description:body.background_description,
+  if (body.background_description) {
+    await historical_bg.create({
+      country_id: newCountry.id,
+      background_description: body.background_description,
     })
-   }
+  }
 
-   return res.status(201).json({
-      status: 'success',
-      data: newCountry
-   });
+  const countryWithBackground = await country.findOne({
+    where: { id: newCountry.id },
+    include: [
+      {
+        model: historical_bg,
+        as: 'histories', // Ensure this alias matches the association
+      },
+    ],
+  });
+
+  return res.status(201).json({
+    status: 'success',
+    data: countryWithBackground,
+  });
 
 });
 
 
 const getAllCountry = catchAsync(async (req, res, next) => {
-   const result = await country.findAll({
-    include:[
+  const result = await country.findAll({
+    include: [
       {
-        model:historical_bg,
+        model: historical_bg,
         as: 'history', // alias for querying
-        attributes:['background_description']
+        attributes: ['background_description']
       }
-  
-    ]
-   });
 
-   res.status(200).json({
-      message: "success",
-      data: result
-   })
+    ]
+  });
+
+  res.status(200).json({
+    message: "success",
+    data: result
+  })
 
 
 });
@@ -65,28 +75,40 @@ const getAllCountry = catchAsync(async (req, res, next) => {
 
 
 const getByQuery = catchAsync(async (req, res, next) => {
-   const { name, iso } = req.query;
- 
-   let result;
-   if (name) {
-     result = await country.findOne({ where: { name: name } });
-     if (!result) {
-       return next(new AppError('No country found with name ' + name, 404));
-     }
-   } else if (iso) {
-     result = await country.findOne({ where: { iso_code: iso } });
-     if (!result) {
-       return next(new AppError('No country found with iso code ' + iso, 404));
-     }
-   } else {
-     return next(new AppError('Please provide a name or iso query', 400));
-   }
- 
-   res.status(200).json({
-     message: 'success',
-     data: result,
-   });
- });
+  const { name, iso } = req.query;
+
+  let result;
+  if (name) {
+    result = await country.findOne({
+      where: { name: name }, include: [{
+        model: historical_bg,
+        as: 'history', // alias for querying
+        attributes: ['background_description']
+      }]
+    });
+    if (!result) {
+      return next(new AppError('No country found with name ' + name, 404));
+    }
+  } else if (iso) {
+    result = await country.findOne({
+      where: { iso_code: iso }, include: [{
+        model: historical_bg,
+        as: 'history', // alias for querying
+        attributes: ['background_description']
+      }]
+    });
+    if (!result) {
+      return next(new AppError('No country found with iso code ' + iso, 404));
+    }
+  } else {
+    return next(new AppError('Please provide a name or iso query', 400));
+  }
+
+  res.status(200).json({
+    message: 'success',
+    data: result,
+  });
+});
 
 
 module.exports = { createCountry, getAllCountry, getByQuery };
--- models/country.js
+++ models/country.js
@@ -5,125 +5,124 @@ const sequelize = require("../config/databaseConfig");
 // const User = require("./user");
 
 
-module.exports = (sequelize)=>{
+module.exports = (sequelize) => {
 
 
-const country= sequelize.define('country',{
-  id:{
-    allowNull:false,
-    autoIncrement:true,
-    primaryKey:true,
-    type:DataTypes.INTEGER
-  },
-  name:{
-    type:DataTypes.STRING,
-    allowNull:false,
-    validate:{
-      notNull:{
-        msg:'Please enter a valid name'
-      },
-      notEmpty:{
-        msg:"Name cannot be empty"
-      },
+  const country = sequelize.define('country', {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: 'Please enter a valid name'
+        },
+        notEmpty: {
+          msg: "Name cannot be empty"
+        },
+      }
+    },
+    iso_code: {
+      type: DataTypes.STRING(2),
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: 'Please enter a valid iso code'
+        },
+        notEmpty: {
+          msg: "Iso code cannot be empty"
+        },
+      }
+    },
+    capital: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: 'Please enter a valid capital'
+        },
+        notEmpty: {
+          msg: "Capital cannot be empty"
+        },
+      }
+    },
+    countryImage: {
+      type: DataTypes.ARRAY(DataTypes.STRING),
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: 'Please enter a valid image'
+        },
+        notEmpty: {
+          msg: "Product image cannot be empty"
+        },
+      }
+    },
+    coastline_km: {
+      type: DataTypes.FLOAT,
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: 'Please enter a valid coastline'
+        },
+        notEmpty: {
+          msg: "Coastline cannot be empty"
+        },
+      }
+    },
+    climate: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: 'Please enter a valid climate'
+        },
+        notEmpty: {
+          msg: "Climate cannot be empty"
+        },
+      }
+    },
+    createdBy: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+      references: {
+        model: 'user',
+        key: 'id'
+      }
+    },
+    createdAt: {
+      allowNull: false,
+      type: DataTypes.DATE
+    },
+    updatedAt: {
+      allowNull: false,
+      type: DataTypes.DATE
+    },
+    deletedAt: {
+      type: DataTypes.DATE
     }
-  },
-  iso_code:{
-    type:DataTypes.STRING(2),
-    allowNull:false,
-    validate:{
-      notNull:{
-        msg:'Please enter a valid iso code'
-      },
-      notEmpty:{
-        msg:"Iso code cannot be empty"
-      },
-    }
-  },
-  capital:{
-    type:DataTypes.STRING,
-    allowNull:false,
-    validate:{
-      notNull:{
-        msg:'Please enter a valid capital'
-      },
-      notEmpty:{
-        msg:"Capital cannot be empty"
-      },
-    }
-  },
-  countryImage:{
-    type:DataTypes.ARRAY(DataTypes.STRING),
-    allowNull:false,
-    validate:{
-      notNull:{
-        msg:'Please enter a valid image'
-      },
-      notEmpty:{
-        msg:"Product image cannot be empty"
-      },
-    }
-  },
-  coastline_km:{
-    type:DataTypes.FLOAT,
-    allowNull:false,
-    validate:{
-      notNull:{
-        msg:'Please enter a valid coastline'
-      },
-      notEmpty:{
-        msg:"Coastline cannot be empty"
-      },
-    }
-  },
-  climate:{
-    type:DataTypes.TEXT,
-    allowNull:false,
-    validate:{
-      notNull:{
-        msg:'Please enter a valid climate'
-      },
-      notEmpty:{
-        msg:"Climate cannot be empty"
-      },
-    }
-  },
-  createdBy:{
-    type:DataTypes.INTEGER,
-    allowNull:true,
-    references:{
-      model: 'user',
-      key:'id'
-    }
-  },
-  createdAt:{
-    allowNull:false,
-    type:DataTypes.DATE
-  },
-  updatedAt:{
-    allowNull:false,
-    type:DataTypes.DATE
-  },
-  deletedAt:{
-    type:DataTypes.DATE
+  }, {
+    paranoid: true,
+    freezeTableName: true,
+    tableName: 'country',
+  })
+
+
+
+
+  country.associate = (models) => {
+    country.belongsTo(models.user, { foreignKey: 'createdBy', });
+
+    country.hasMany(models.historical_bg, { foreignKey: 'country_id', as: 'history', })
   }
-},{
-  paranoid:true,
-  freezeTableName:true,
-  tableName:'country',
-})
 
 
+  return country;
 
 
-country.associate = (models)=>{
-  country.belongsTo(models.user, {foreignKey: 'createdBy', });
-
-  country.hasMany(models.historical_bg,{foreignKey:'country_id',as: 'history',})
 }
-
-
-return country;
-
- 
-}
- 
