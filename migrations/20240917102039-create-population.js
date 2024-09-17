02a7bf546079f29a0f1bbbbb742825b28326e5f0
Girish keleng <girishkeleng30@gmail.com>
2024-09-17 16:04:49 +0530

    Add migration and model for population table

---
+++ migrations/20240917102039-create-population.js
@@ -0,0 +1,61 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('populations', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      country_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references:{
+          model: 'country',
+          key: 'id'
+        },
+      },
+      total_population: {
+        type: Sequelize.BIGINT,
+        allowNull: false,
+      },
+      male_population: {
+        type: Sequelize.BIGINT,
+        allowNull: true,
+      },
+      female_population: {
+        type: Sequelize.BIGINT,
+        allowNull: true,
+      },
+      population_estimate_year: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      female_comparison_ranking:{
+        type: Sequelize.INTEGER,
+        allowNull: true,
+      },
+      male_comparison_ranking:{
+        type:Sequelize.INTEGER,
+        allowNull:true,
+      },
+      total_comparison_ranking:{
+        type:Sequelize.INTEGER,
+        allowNull:false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('populations');
+  }
+};
