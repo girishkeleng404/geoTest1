9b311894096872e8f31917f91a167cddc0e91a7f
Girish keleng <girishkeleng30@gmail.com>
2024-09-18 18:52:33 +0530

    Refactor the nationality model for better readality

--- models/nationality.js
+++ models/nationality.js
@@ -1,23 +1,75 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
-module.exports = (sequelize, DataTypes) => {
-  class nationality extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
+
+const { DataTypes } = require("sequelize");
+const { sequelize } = require(".");
+
+
+
+module.exports = (sequelize) => {
+  const nationality = sequelize.define('nationality', {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER,
+    },
+    country_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'country',
+        key: "id",
+      },
+
+      validate: {
+        notNull: {
+          msg: "Please enter a valid country id"
+        },
+        notEmpty: {
+          msg: 'Country id cannot be empty'
+        }
+      }
+    },
+    nationality: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: "Please enter a valid input"
+        },
+        notEmpty: {
+          msg: "Empty value is not allowed"
+        }
+      },
+    },
+    ethnic_groups: {
+      type: DataTypes.TEXT,
+      allowNull: true,
+    },
+    population_distribution: {
+      type: DataTypes.TEXT,
+      allowNull: true,
+    },
+    demographic_profile: {
+      type: DataTypes.TEXT,
+      allowNull: true,
+    },
+    createdAt: {
+      allowNull: false,
+      type: DataTypes.DATE
+    },
+    updatedAt: {
+      allowNull: false,
+      type: DataTypes.DATE
+    },
+    deletedAt: {
+      type: DataTypes.DATE,
     }
-  }
-  nationality.init({
-    nationality: DataTypes.TEXT
-  }, {
-    sequelize,
-    modelName: 'nationality',
-  });
+
+
+  })
+
+
+
   return nationality;
-};
+}
