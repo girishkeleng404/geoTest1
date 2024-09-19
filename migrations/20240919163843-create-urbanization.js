25bf5bb308a00628790e135dc7758833f7e4a7cb
Girish keleng <girishkeleng30@gmail.com>
2024-09-19 22:14:36 +0530

    Add migration and model for urbanization

---
+++ migrations/20240919163843-create-urbanization.js
@@ -0,0 +1,49 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('urbanization', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      country_id: {
+        type: Sequelize.INTEGER,
+        allowNull:false,
+        references:{
+          model: 'country',
+          key: 'id',
+        }
+      },
+      urban_population: {
+        type: Sequelize.TEXT,
+        allowNull:false,
+      },
+     rate_of_urbanization: {
+        type: Sequelize.TEXT,
+        allowNull:true,
+      },
+      major_urban_areas_population: {
+        type: Sequelize.TEXT,
+        allowNull:true,
+      },
+      
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      deletedAt:{
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('urbanization');
+  }
+};
