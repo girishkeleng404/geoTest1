b46138fe82f84894154480ad4b127f247d1d6677
Girish keleng <girishkeleng30@gmail.com>
2024-09-20 20:14:06 +0530

    Add migration and model for education_data

---
+++ migrations/20240920143817-create-education-data.js
@@ -0,0 +1,49 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('education_data', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      country_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'country',
+          key: "id"
+        }
+      },
+      education_expenditure: {
+        type: Sequelize.JSONB,
+        allowNull: true,
+      },
+      literacy_rate: {
+        type: Sequelize.JSONB,
+        allowNull: true,
+      },
+      school_life_expectancy: {
+        type: Sequelize.JSONB,
+        allowNull: true,
+      },
+
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      deletedAt: {
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('education_data');
+  }
+};
