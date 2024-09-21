b2c9f0dc023499ef742d9d4c9d2db5ef1ed43de6
Girish keleng <girishkeleng30@gmail.com>
2024-09-21 10:20:58 +0530

    splitting the logic into separate files

--- controllers/countryController.js
+++ controllers/countryController.js
@@ -1,14 +1,8 @@
 const catchAsync = require('../utils/catchError');
-const { user, historical_bg, population, nationality,language_religion,age_structure, dependency_ratio,population_rate, urbanization,sex_marriage,health_data, education_data } = require('../models');
+const { user, historical_bg, population, nationality, language_religion, age_structure, dependency_ratio, population_rate, urbanization, sex_marriage, health_data, education_data } = require('../models');
 const { country } = require('../models');
 const AppError = require("../utils/appError");
- 
- 
- 
- 
- 
- 
-
+const { populationService } = require('./service/countryService');
 
 
 
@@ -19,7 +13,7 @@ const createCountry = catchAsync(async (req, res, next) => {
   const userId = req.user.id;
   console.log(req.user);
   const userType = req.user.userType;
-   
+
 
   if (userType !== '0') {
     return next(new AppError('You are not authorized to create a country', 403));
@@ -35,144 +29,18 @@ const createCountry = catchAsync(async (req, res, next) => {
     createdBy: userId,
   });
 
-  if (body.background_description || body.total_population || body.nationality || body.languages|| body.age_0_14 || body.total_dependency_ratio || body.population_growth_rate || body.urban_population || body.sex_ratio || body.current_health_expenditure || body.education_expenditure ) {
 
-    if (body.background_description) {
+  if (body.background_description) {
 
-      await historical_bg.create({
-        country_id: newCountry.id,
-        background_description: body.background_description,
-      })
-    }
-
-    if (body.total_population) {
-    const populationData =  await population.create({
-        country_id: newCountry.id,
-        total_population: body.total_population,
-        male_population: body.male_population,
-        female_population: body.female_population,
-        population_estimate_year: body.population_estimate_year,
-        female_comparison_ranking: body.female_comparison_ranking,
-        male_comparison_ranking: body.male_comparison_ranking,
-        total_comparison_ranking: body.total_comparison_ranking,
-      });
-    
-
-
-    if (body.nationality) {
-      await nationality.create({
-        country_id: newCountry.id,
-        nationality: body.nationality,
-        ethnic_groups: body.ethnic_groups,
-        population_distribution: body.population_distribution,
-        demographic_profile: body.demographic_profile,
-
-      })
-    };
-
-    if(body.languages){
-      await language_religion.create({
-        country_id: newCountry.id,
-        languages: body.languages,
-        major_language_sample:body.major_language_sample,
-        notes:body.notes,
-        religions:body. religions
-      })
-    };
-
-    if(body.age_0_14){
-      await age_structure.create({
-        population_id: populationData.country_id,
-        age_0_14:body.age_0_14,
-        age_15_64:body.age_15_64,
-        age_65_plus:body.age_65_plus,
-        estimated_year:body.estimated_year
-      })
-    };
-    if(body.total_dependency_ratio){
-      await dependency_ratio.create({
-        country_id:newCountry.id,
-        total_dependency_ratio:body.total_dependency_ratio,
-        youth_dependency_ratio:body.youth_dependency_ratio,
-        elderly_dependency_ratio:body.elderly_dependency_ratio,
-        potential_support_ratio:body.potential_support_ratio,
-        dependency_estimated_year:body.dependency_estimated_year
-      })
-    };
-    if(body.population_growth_rate){
-      
-      await population_rate.create({
-        country_id:newCountry.id,
-        population_rate: body.population_rate,
-        population_growth_rate_rank:body. population_growth_rate_rank,
-        birth_rate:body.birth_rate,
-        birth_rate_rank:body.birth_rate_rank,
-        death_rate:body.death_rate,
-        death_rate_rank:body.death_rate_rank,
-        total_fertility_rate:body.total_fertility_rate,
-        total_fertility_rate_rank:body.total_fertility_rate_rank,
-        gross_reproduction_rate:body.gross_reproduction_rate,
-        gross_reproduction_rate_rank:body.gross_reproduction_rate_rank,
-        obesity_rate:body.obesity_rate,
-        obesity_rate_rank:body.obesity_rate_rank,
-        net_migration_rate:body.net_migration_rate,
-        net_migration_rate_rank:body.net_migration_rate_rank
-
-      })
-    };
-
-    if(body.urban_population){
-      await urbanization.create({
-        country_id:newCountry.id,
-        urban_population:body.urban_population,
-        rate_of_urbanization:body.rate_of_urbanization,
-        major_urban_areas_population:body.major_urban_areas_population
-
-      })
-    };
-    
-    if(body.sex_ratio){
-      await sex_marriage.create({
-        country_id:newCountry.id,
-        sex_ratio:body.sex_ratio,
-        mother_age_at_first_birth:body.mother_age_at_first_birth,
-        currently_married_womens_15to52:body.currently_married_womens_15to52,
-        women_married_by_age_15:body.women_married_by_age_15,
-        women_married_by_age_18:body.women_married_by_age_18,
-        contraceptive_prevalence_rate:body.contraceptive_prevalence_rate
-      })
-    };
-
-    if(body.current_health_expenditure){
-      await health_data.create({
-        country_id:newCountry.id,
-        current_health_expenditure:body.current_health_expenditure,
-        life_expectancy:body.life_expectancy,
-        infant_mortality:body.infant_mortality,
-        maternal_mortality:body.maternal_mortality,
-        physician_density:body.physician_density,
-        hospital_bed_density:body.hospital_bed_density,
-        drinking_water:body.drinking_water,
-        sanitation:body.sanitation,
-        major_infectious_diseases:body.major_infectious_diseases,
-        adult_obesity:body.adult_obesity,
-        underweight_children:body.underweight_children
-      })
-    };
-    
-    if(body.education_expenditure){
-      await education_data.create({
-        country_id:newCountry.id,
-        education_expenditure:body.education_expenditure,
-        literacy_rate:body.literacy_rate,
-        school_life_expectancy:body.school_life_expectancy
-      })
-    };
+    await historical_bg.create({
+      country_id: newCountry.id,
+      background_description: body.background_description,
+    })
   }
+  // population data
+  await populationService(body,newCountry.id);
 
 
-
-  }
 
   const countryWithBackground = await country.findOne({
     where: { id: newCountry.id },
@@ -200,15 +68,15 @@ const createCountry = catchAsync(async (req, res, next) => {
           },
           {
             model: age_structure,
-            as:'age_structure'
+            as: 'age_structure'
           },
           {
             model: dependency_ratio,
-            as:'dependency_ratio'
+            as: 'dependency_ratio'
           },
           {
             model: urbanization,
-            as:'urbanization_Data'
+            as: 'urbanization_Data'
           },
           {
             model: sex_marriage,
@@ -234,7 +102,6 @@ const createCountry = catchAsync(async (req, res, next) => {
   });
 
 });
-
 
 
 
@@ -308,7 +175,6 @@ const countryIncludes = [
 
 
 
-
 const getAllCountry = catchAsync(async (req, res, next) => {
   const result = await country.findAll({
     include: countryIncludes,
@@ -318,7 +184,6 @@ const getAllCountry = catchAsync(async (req, res, next) => {
     message: "success",
     data: result
   })
-
 
 });
 
@@ -332,14 +197,14 @@ const getByQuery = catchAsync(async (req, res, next) => {
   let result;
   if (name) {
     result = await country.findAll({
-      where: { name: name },  include:  countryIncludes
+      where: { name: name }, include: countryIncludes
     });
     if (!result) {
       return next(new AppError('No country found with name ' + name, 404));
     }
   } else if (iso) {
     result = await country.findOne({
-      where: { iso_code: iso },  include: countryIncludes
+      where: { iso_code: iso }, include: countryIncludes
     });
     if (!result) {
       return next(new AppError('No country found with iso code ' + iso, 404));
--- controllers/service/countryService.js
+++ controllers/service/countryService.js
@@ -1,3 +1,135 @@
-const { user, historical_bg, population, nationality,language_religion,age_structure, dependency_ratio,population_rate, urbanization,sex_marriage,health_data, education_data } = require('../../models');
+const { population, nationality,language_religion,age_structure, dependency_ratio,population_rate, urbanization,sex_marriage,health_data, education_data } = require('../../models');
 
 
+const populationService = async(body,countryId)=> {
+    
+    if (body.total_population) {
+        const populationData =  await population.create({
+            country_id: countryId,
+            total_population: body.total_population,
+            male_population: body.male_population,
+            female_population: body.female_population,
+            population_estimate_year: body.population_estimate_year,
+            female_comparison_ranking: body.female_comparison_ranking,
+            male_comparison_ranking: body.male_comparison_ranking,
+            total_comparison_ranking: body.total_comparison_ranking,
+          });
+        
+    
+    
+        if (body.nationality) {
+          await nationality.create({
+            population_id: populationData.id,
+            nationality: body.nationality,
+            ethnic_groups: body.ethnic_groups,
+            population_distribution: body.population_distribution,
+            demographic_profile: body.demographic_profile,
+    
+          })
+        };
+    
+        if(body.languages){
+          await language_religion.create({
+            population_id: populationData.id,
+            languages: body.languages,
+            major_language_sample:body.major_language_sample,
+            notes:body.notes,
+            religions:body. religions
+          })
+        };
+    
+        if(body.age_0_14){
+          await age_structure.create({
+            population_id: populationData.id,
+            age_0_14:body.age_0_14,
+            age_15_64:body.age_15_64,
+            age_65_plus:body.age_65_plus,
+            estimated_year:body.estimated_year
+          })
+        };
+        if(body.total_dependency_ratio){
+          await dependency_ratio.create({
+            population_id: populationData.id,
+            total_dependency_ratio:body.total_dependency_ratio,
+            youth_dependency_ratio:body.youth_dependency_ratio,
+            elderly_dependency_ratio:body.elderly_dependency_ratio,
+            potential_support_ratio:body.potential_support_ratio,
+            dependency_estimated_year:body.dependency_estimated_year
+          })
+        };
+        if(body.population_growth_rate){
+          
+          await population_rate.create({
+            population_id: populationData.id,
+            population_rate: body.population_rate,
+            population_growth_rate_rank:body. population_growth_rate_rank,
+            birth_rate:body.birth_rate,
+            birth_rate_rank:body.birth_rate_rank,
+            death_rate:body.death_rate,
+            death_rate_rank:body.death_rate_rank,
+            total_fertility_rate:body.total_fertility_rate,
+            total_fertility_rate_rank:body.total_fertility_rate_rank,
+            gross_reproduction_rate:body.gross_reproduction_rate,
+            gross_reproduction_rate_rank:body.gross_reproduction_rate_rank,
+            obesity_rate:body.obesity_rate,
+            obesity_rate_rank:body.obesity_rate_rank,
+            net_migration_rate:body.net_migration_rate,
+            net_migration_rate_rank:body.net_migration_rate_rank
+    
+          })
+        };
+    
+        if(body.urban_population){
+          await urbanization.create({
+            population_id: populationData.id,
+            urban_population:body.urban_population,
+            rate_of_urbanization:body.rate_of_urbanization,
+            major_urban_areas_population:body.major_urban_areas_population
+    
+          })
+        };
+        
+        if(body.sex_ratio){
+          await sex_marriage.create({
+            population_id: populationData.id,
+            sex_ratio:body.sex_ratio,
+            mother_age_at_first_birth:body.mother_age_at_first_birth,
+            currently_married_womens_15to52:body.currently_married_womens_15to52,
+            women_married_by_age_15:body.women_married_by_age_15,
+            women_married_by_age_18:body.women_married_by_age_18,
+            contraceptive_prevalence_rate:body.contraceptive_prevalence_rate
+          })
+        };
+    
+        if(body.current_health_expenditure){
+          await health_data.create({
+            population_id: populationData.id,
+            current_health_expenditure:body.current_health_expenditure,
+            life_expectancy:body.life_expectancy,
+            infant_mortality:body.infant_mortality,
+            maternal_mortality:body.maternal_mortality,
+            physician_density:body.physician_density,
+            hospital_bed_density:body.hospital_bed_density,
+            drinking_water:body.drinking_water,
+            sanitation:body.sanitation,
+            major_infectious_diseases:body.major_infectious_diseases,
+            adult_obesity:body.adult_obesity,
+            underweight_children:body.underweight_children
+          })
+        };
+        
+        if(body.education_expenditure){
+          await education_data.create({
+            population_id: populationData.id,
+            education_expenditure:body.education_expenditure,
+            literacy_rate:body.literacy_rate,
+            school_life_expectancy:body.school_life_expectancy
+          })
+        };
+      }
+}
+
+
+
+
+module.exports ={populationService}
