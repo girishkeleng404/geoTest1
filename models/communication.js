6db086c07cfafbae84941863b23091fe2f8e56af
Girish keleng <girishkeleng30@gmail.com>
2024-09-28 21:46:40 +0530

    Refactor code to rename communication table and model

--- models/communication.js
+++ models/communication.js
@@ -1,23 +1,99 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
-module.exports = (sequelize, DataTypes) => {
-  class communication extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
+
+const { DataTypes } = require("sequelize");
+
+
+module.exports =(sequelize)=>{
+  const communication = sequelize.define('communication',{
+          id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER
+      },
+      country_id: {
+        type: DataTypes.INTEGER,
+        allowNull:false,
+        references:{
+          model: 'country',
+          key: 'id',
+        },
+        onDelete:CASCADE,
+        onUpdate:CASCADE,
+      },
+      telephone_subscription_in_millions:{
+        type: DataTypes.FLOAT,
+        allowNull: true,
+      },
+      telephone_subscription_per_100:{
+        type: DataTypes.FLOAT,
+        allowNull: true,
+      },
+      mobile_subscription_in_millions:{
+        type: DataTypes.FLOAT,
+        allowNull: true,
+      },
+      mobile_subscription_per_100:{
+        type: DataTypes.FLOAT,
+        allowNull: true,
+      },
+      mobile_subscription_ranking:{
+        type: DataTypes.INTEGER,
+        allowNull: true,
+      },
+      telecommunication_system:{
+        type: DataTypes.ARRAY(DataTypes.STRING),
+        allowNull: true,
+      },
+      broadcast_media:{
+        type: DataTypes.TEXT,
+        allowNull:true,
+      },
+      internet_country_code:{
+        type: DataTypes.STRING,
+        allowNull:true,
+      },
+      internet_users_in_millions:{
+        type: DataTypes.FLOAT,
+        allowNull:true,
+      },
+      internet_users_percentage:{
+        type: DataTypes.FLOAT,
+        allowNull:true,
+      },
+      internet_users_ranking:{
+        type: DataTypes.INTEGER,
+        allowNull:true,
+      },
+      broadband_subscription_in_millions:{
+        type: DataTypes.STRING,
+        allowNull:true,
+      },
+      createdAt: {
+        allowNull: false,
+        type: DataTypes.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: DataTypes.DATE
+      },
+      deletedAt:{
+        type: DataTypes.DATE,
+        allowNull:true,
+      }
+  },{
+    paranoid: true,
+    freezeTableName: true,
+    tableName: 'communication',
+  })
+
+
+  communication.associate =(models)=>{
+    communication.belongsTo(models.country,{
+      foreignKey: 'country_id',
+      as: 'country',
+    })
   }
-  communication.init({
-    country_id: DataTypes.INTEGER
-  }, {
-    sequelize,
-    modelName: 'communication',
-  });
+
   return communication;
-};
+}
