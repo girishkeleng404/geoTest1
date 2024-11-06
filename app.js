326a3334d98de57aa00a2d56b37797f9d093c896
girishkeleng404 <girishkeleng30@gmail.com>
2024-11-06 20:53:30 +0530

    add a postman data and refactore .gitignore

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
-const express = require('express');
-const env = require('dotenv');
-const authRoute = require('./routes/authRoute');
-const countryRoute = require('./routes/countryRoute')
-const globleErrorHandler = require('./controllers/errorController');
-
+const express = require("express");
+const env = require("dotenv");
+const authRoute = require("./routes/authRoute");
+const countryRoute = require("./routes/countryRoute");
+const globleErrorHandler = require("./controllers/errorController");
 
 const app = express();
 env.config();
@@ -11,39 +10,28 @@ const port = process.env.APP_PORT;
 
 app.use(express.json());
 
-app.get('/',async(req,res)=>{
-    res.status(200).json({
-        message: "success",
-    })
-})
-
-
-app.use('/api/v1/auth',authRoute);
-
-app.use('/api/v1/country',countryRoute);
-
-
-
-
-
-const { sequelize } = require('./models');
-sequelize.sync({ force: false })  // Set force: true to drop and recreate tables, but careful with data loss.
-  .then(() => {
-    console.log('Database synced successfully');
-  })
-  .catch(error => {
-    console.error('Error syncing database:', error);
+app.get("/", async (req, res) => {
+  res.status(200).json({
+    message: "success",
   });
+});
 
+app.use("/api/v1/auth", authRoute);
+
+app.use("/api/v1/country", countryRoute);
+
+const { sequelize } = require("./models");
+sequelize
+  .sync({ force: false }) // Set force: true to drop and recreate tables, but careful with data loss.
+  .then(() => {
+    console.log("Database synced successfully");
+  })
+  .catch((error) => {
+    console.error("Error syncing database:", error);
+  });
 
 app.use(globleErrorHandler);
 
-
-
-
-
-
-
-app.listen(port,()=>{
-    console.log(`Server is running on port ${port}`);
-})
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
