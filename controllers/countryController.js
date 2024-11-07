b91d0d185d3488217828bc0cd5a8d1a630f16f10
girishkeleng404 <girishkeleng30@gmail.com>
2024-11-07 20:06:56 +0530

    refactor code for better readability

--- controllers/countryController.js
+++ controllers/countryController.js
@@ -1,14 +1,57 @@
-const catchAsync = require('../utils/catchError');
-const { user, historical_bg, population, nationality, language_religion, age_structure, dependency_ratio, population_rate, urbanization, sex_marriage, health_data, education_data, substance_use_data, environment, government, legal_law_data, government_more, economy, gdp_data, agricultural_and_industrial_data, labor_market_data, household_inco_expe_data, public_finance_debt_data, trade_data, debt_ext_exchange_rate, energy, communication, transportation, military, space, terrorism, transnational_issues, sequelize, Sequelize } = require('../models');
-const { country, recursiveSoftDelete } = require('../models');
+const catchAsync = require("../utils/catchError");
+const {
+  user,
+  historical_bg,
+  population,
+  nationality,
+  language_religion,
+  age_structure,
+  dependency_ratio,
+  population_rate,
+  urbanization,
+  sex_marriage,
+  health_data,
+  education_data,
+  substance_use_data,
+  environment,
+  government,
+  legal_law_data,
+  government_more,
+  economy,
+  gdp_data,
+  agricultural_and_industrial_data,
+  labor_market_data,
+  household_inco_expe_data,
+  public_finance_debt_data,
+  trade_data,
+  debt_ext_exchange_rate,
+  energy,
+  communication,
+  transportation,
+  military,
+  space,
+  terrorism,
+  transnational_issues,
+  sequelize,
+  Sequelize,
+} = require("../models");
+const { country, recursiveSoftDelete } = require("../models");
 const AppError = require("../utils/appError");
-const { populationService, environmentService, governmentService, economyService, countryIncludes, energyService, communicationService, transportationService, militaryService, spaceService, terrorismService, transnational_issuesService } = require('./service/countryService');
-const { where } = require('sequelize');
-
-
-
-
-
+const {
+  populationService,
+  environmentService,
+  governmentService,
+  economyService,
+  countryIncludes,
+  energyService,
+  communicationService,
+  transportationService,
+  militaryService,
+  spaceService,
+  terrorismService,
+  transnational_issuesService,
+} = require("./service/countryService");
+const { where } = require("sequelize");
 
 const createCountry = catchAsync(async (req, res, next) => {
   const body = req.body;
@@ -16,9 +59,10 @@ const createCountry = catchAsync(async (req, res, next) => {
   console.log(req.user);
   const userType = req.user.userType;
 
-
-  if (userType !== '0') {
-    return next(new AppError('You are not authorized to create a country', 403));
+  if (userType !== "0") {
+    return next(
+      new AppError("You are not authorized to create a country", 403),
+    );
   }
 
   const newCountry = await country.create({
@@ -31,13 +75,11 @@ const createCountry = catchAsync(async (req, res, next) => {
     createdBy: userId,
   });
 
-
   if (body.background_description) {
-
     await historical_bg.create({
       country_id: newCountry.id,
       background_description: body.background_description,
-    })
+    });
   }
   // Insert data
   await populationService(body, newCountry.id);
@@ -47,172 +89,158 @@ const createCountry = catchAsync(async (req, res, next) => {
   await energyService(body, newCountry.id);
   await communicationService(body, newCountry.id);
   await transportationService(body, newCountry.id);
-  await militaryService(body, newCountry.id)
+  await militaryService(body, newCountry.id);
   await spaceService(body, newCountry.id);
-  await terrorismService(body, newCountry.id)
-  await transnational_issuesService(body, newCountry.id)
-
-
-
+  await terrorismService(body, newCountry.id);
+  await transnational_issuesService(body, newCountry.id);
 
   const countryWithBackground = await country.findOne({
     where: { id: newCountry.id },
     include: [
       {
         model: historical_bg,
-        as: 'history', // Ensure this alias matches the association
+        as: "history", // Ensure this alias matches the association
       },
       {
         model: population,
-        as: 'populationData',
+        as: "populationData",
 
         include: [
           {
             model: population_rate,
-            as: 'population_rate_Data'
+            as: "population_rate_Data",
           },
           {
             model: nationality,
-            as: 'nationality'
+            as: "nationality",
           },
           {
             model: language_religion,
-            as: 'language_religion'
+            as: "language_religion",
           },
           {
             model: age_structure,
-            as: 'age_structure'
+            as: "age_structure",
           },
           {
             model: dependency_ratio,
-            as: 'dependency_ratio'
+            as: "dependency_ratio",
           },
           {
             model: urbanization,
-            as: 'urbanization_Data'
+            as: "urbanization_Data",
           },
           {
             model: sex_marriage,
-            as: 'sex_marriage_Data'
+            as: "sex_marriage_Data",
           },
           {
             model: health_data,
-            as: 'health_data'
+            as: "health_data",
           },
           {
             model: education_data,
-            as: 'education_data'
+            as: "education_data",
           },
           {
             model: substance_use_data,
-            as: 'substance_use_data'
-          }
-
-        ]
+            as: "substance_use_data",
+          },
+        ],
       },
       {
         model: environment,
-        as: 'environment_data'
+        as: "environment_data",
       },
       {
         model: government,
-        as: 'government_data',
+        as: "government_data",
 
         include: [
           {
             model: legal_law_data,
-            as: 'legal_law_data'
+            as: "legal_law_data",
           },
           {
             model: government_more,
-            as: 'gov_more'
-          }
-        ]
+            as: "gov_more",
+          },
+        ],
       },
       {
         model: economy,
-        as: 'economy_data',
+        as: "economy_data",
 
         include: [
           {
             model: gdp_data,
-            as: 'gdp_data'
+            as: "gdp_data",
           },
           {
             model: agricultural_and_industrial_data,
-            as: 'agricultural_and_industrial_data'
+            as: "agricultural_and_industrial_data",
           },
           {
             model: labor_market_data,
-            as: 'labor_market_data'
+            as: "labor_market_data",
           },
           {
             model: household_inco_expe_data,
-            as: 'household_inco_expe_data'
+            as: "household_inco_expe_data",
           },
 
           {
             model: public_finance_debt_data,
-            as: 'public_finance_debt_data',
-
+            as: "public_finance_debt_data",
           },
           {
             model: trade_data,
-            as: 'trade_data'
+            as: "trade_data",
           },
           {
             model: debt_ext_exchange_rate,
-            as: 'debt_ext_exchange_rate',
-
-          }
-        ]
+            as: "debt_ext_exchange_rate",
+          },
+        ],
       },
       {
         model: energy,
-        as: 'energy_data',
+        as: "energy_data",
       },
       {
         model: communication,
-        as: 'communication_data',
+        as: "communication_data",
       },
       {
         model: transportation,
-        as: 'transportation_data'
+        as: "transportation_data",
       },
       {
         model: military,
-        as: 'military_data'
+        as: "military_data",
       },
       {
         model: space,
-        as: 'space_data'
+        as: "space_data",
       },
       {
         model: terrorism,
-        as: 'terrorism_data'
+        as: "terrorism_data",
       },
       {
         model: transnational_issues,
-        as: 'transnational_issues_data',
-      }
+        as: "transnational_issues_data",
+      },
     ],
   });
 
   return res.status(201).json({
-    status: 'success',
+    status: "success",
     data: countryWithBackground,
   });
-
 });
 
-
-
 // -------------------xxxx------------------------
-
-
-
-
-
 
 const getAllCountry = catchAsync(async (req, res, next) => {
   const result = await country.findAll({
@@ -223,14 +251,11 @@ const getAllCountry = catchAsync(async (req, res, next) => {
   //  console.log(result)
   res.status(200).json({
     message: "success",
-    data: result
-  })
-
+    data: result,
+  });
 });
 
-
 // ----------------------xxxxxxxxx---------------------
-
 
 const getByQuery = catchAsync(async (req, res, next) => {
   const { name, iso } = req.query;
@@ -238,105 +263,59 @@ const getByQuery = catchAsync(async (req, res, next) => {
   let result;
   if (name) {
     result = await country.findAll({
-      where: { name: name }, include: countryIncludes
+      where: { name: name },
+      include: countryIncludes,
     });
     if (!result) {
-      return next(new AppError('No country found with name ' + name, 404));
+      return next(new AppError("No country found with name " + name, 404));
     }
   } else if (iso) {
     result = await country.findOne({
-      where: { iso_code: iso }, include: countryIncludes
+      where: { iso_code: iso },
+      include: countryIncludes,
     });
     if (!result) {
-      return next(new AppError('No country found with iso code ' + iso, 404));
+      return next(new AppError("No country found with iso code " + iso, 404));
     }
   } else {
-    return next(new AppError('Please provide a name or iso query', 400));
+    return next(new AppError("Please provide a name or iso query", 400));
   }
 
   res.status(200).json({
-    message: 'success',
+    message: "success",
     data: result,
   });
 });
 
 // ----------------xxxxxxxxxxxxxxxxx------------------
 
-
-
-
 const deleteISO = catchAsync(async (req, res, next) => {
   const { iso_code } = req.query;
 
   if (!iso_code) {
-    return next(new AppError('no iso_code is provided', 400));
-  };
+    return next(new AppError("no iso_code is provided", 400));
+  }
 
-  const result = await country.findOne({ where: { iso_code } })
+  const result = await country.findOne({ where: { iso_code } });
 
   if (!result) {
-    return next(new AppError('No project found with this id', 400));
+    return next(new AppError("No project found with this id", 400));
   }
 
   await result.destroy({ force: true });
 
-
   return res.json({
-    status: 'success',
-    message: 'Country has been deleted'
-  })
-})
+    status: "success",
+    message: "Country has been deleted",
+  });
+});
 
 // ----------------xxxxxxxxxxxxxxxx------------------
 
-
-
-
-// const updateCountry = async (req, res, next) => {
-//   const { iso_code } = req.query;
-//   const countryData = req.body;
-
-//   const transaction = await sequelize.transaction();
-
-//   try {
-//     // Similar update logic with all update operations wrapped in the transaction
-//     const countryInstance = await country.findOne({ where: { iso_code }, include: countryIncludes, transaction });
-
-//     // Update country and associated tables here, using the transaction
-//     await countryInstance.update(countryData, { transaction });
-
-//     //  if (countryData.background_description) {
-//     //   await countryInstance.historical_bg.update({
-//     //     background_description: countryData.background_description,
-//     //   }, { transaction });
-//     // }
-
-//     console.log('Country Data:', countryData);
-//     console.log(countryInstance)
-//      // Check if historical background exists and update it
-//     if (countryData.background_description || countryInstance.historical_bg) {
-//       await countryInstance.historical_bg.update({
-//         background_description: countryData.background_description,
-//       }, { transaction });
-//     }  
-
-
-//     // Commit the transaction if all operations succeed
-//     await transaction.commit();
-
-//     return res.status(200).json({ message: 'Country updated successfully' });
-//   } catch (error) {
-//     // Rollback transaction on failure
-//     await transaction.rollback();
-//     next(error);
-//   }
-
-
-// };
-
 const updateNestedAssociations = async (instance, data, transaction) => {
   for (const nestedKey in data) {
-    if (!instance[nestedKey] || typeof instance[nestedKey] !== 'object') continue;
+    if (!instance[nestedKey] || typeof instance[nestedKey] !== "object")
+      continue;
     if (Array.isArray(instance[nestedKey])) {
       for (let i = 0; i < instance[nestedKey].length; i++) {
         const nestedInstance = instance[nestedKey][i];
@@ -350,7 +329,6 @@ const updateNestedAssociations = async (instance, data, transaction) => {
     }
   }
 };
-
 
 const updateCountry = async (req, res, next) => {
   const { iso_code } = req.query;
@@ -375,9 +353,9 @@ const updateCountry = async (req, res, next) => {
 
     // Iterate over associated tables and update them
     for (const key in countryData) {
-      const associatedData = countryData[key]; 
+      const associatedData = countryData[key];
 
-      if (!countryInstance[key] || typeof countryInstance[key] !== 'object') {
+      if (!countryInstance[key] || typeof countryInstance[key] !== "object") {
         console.log(`${key} is not a valid association`);
         continue;
       }
@@ -387,7 +365,11 @@ const updateCountry = async (req, res, next) => {
           const associatedInstance = countryInstance[key][i];
           const nestedData = associatedData[i];
           if (associatedInstance && nestedData) {
-            await updateNestedAssociations(associatedInstance, nestedData, transaction);
+            await updateNestedAssociations(
+              associatedInstance,
+              nestedData,
+              transaction,
+            );
           }
         }
       } else if (associatedData) {
@@ -398,13 +380,19 @@ const updateCountry = async (req, res, next) => {
     // Commit the transaction
     await transaction.commit();
 
-    return res.status(200).json({ message: 'Country and related data updated successfully' });
+    return res
+      .status(200)
+      .json({ message: "Country and related data updated successfully" });
   } catch (error) {
     await transaction.rollback();
     next(error);
   }
 };
 
-
-
-module.exports = { createCountry, getAllCountry, getByQuery, deleteISO, updateCountry };
+module.exports = {
+  createCountry,
+  getAllCountry,
+  getByQuery,
+  deleteISO,
+  updateCountry,
+};
--- models/index.js
+++ models/index.js
@@ -1,58 +1,50 @@
-'use strict';
+"use strict";
 
-const fs = require('fs');
-const path = require('path');
-const Sequelize = require('sequelize');
-const process = require('process');
+const fs = require("fs");
+const path = require("path");
+const Sequelize = require("sequelize");
+const process = require("process");
 const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
-// const configPath = __dirname + '/../config/config.js';
-// console.log('Loading config from:', configPath);
-// const config = require(configPath)[env];
-const config = require(__dirname + '/../config/config.js')[env];
- 
+const env = process.env.NODE_ENV || "development";
+const config = require(__dirname + "/../config/config.js")[env];
+
 const db = {};
-
-
-
 
 let sequelize;
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
+  sequelize = new Sequelize(
+    config.database,
+    config.username,
+    config.password,
+    config,
+  );
 }
 
-fs
-  .readdirSync(__dirname)
-  .filter(file => {
+fs.readdirSync(__dirname)
+  .filter((file) => {
     return (
-      file.indexOf('.') !== 0 &&
+      file.indexOf(".") !== 0 &&
       file !== basename &&
-      file.slice(-3) === '.js' &&
-      file.indexOf('.test.js') === -1 
+      file.slice(-3) === ".js" &&
+      file.indexOf(".test.js") === -1
     );
   })
-  .forEach(file => {
-    console.log('Loading model file:', file); // Add this line
-    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
+  .forEach((file) => {
+    console.log("Loading model file:", file); // Add this line
+    const model = require(path.join(__dirname, file))(
+      sequelize,
+      Sequelize.DataTypes,
+    );
     db[model.name] = model;
   });
 
-Object.keys(db).forEach(modelName => {
+Object.keys(db).forEach((modelName) => {
   if (db[modelName].associate) {
     db[modelName].associate(db);
   }
 });
-
-
-// const {user,country} = require('./associate');
-// db.user= user;
-// db.country = country;
-
-// db.user = require('./user')(sequelize, Sequelize.DataTypes);
-// db.country = require('./country')(sequelize, Sequelize.DataTypes);
-// db.historical_bg = require('./historical_bg')(sequelize, Sequelize.DataTypes);
 
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
