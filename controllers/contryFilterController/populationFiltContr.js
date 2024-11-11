f78a187dc623b05cf1fd44aa6a41d8ef5e852009
girishkeleng404 <girishkeleng30@gmail.com>
2024-11-11 21:25:57 +0530

    refactore code in filterPopulationController

--- controllers/contryFilterController/populationFiltContr.js
+++ controllers/contryFilterController/populationFiltContr.js
@@ -14,38 +14,35 @@ const getCountriesByPopulation = catchAsync(async (req, res, next) => {
 
   const tp = 1409128297;
   const populationFilter = {};
-  if (minPopulation) populationFilter[Op.gte] = BigInt(minPopulation);
-  if (maxPopulation) populationFilter[Op.lte] = BigInt(maxPopulation);
-
+  if (minPopulation) populationFilter[Op.gte] = minPopulation.toString();
+  if (maxPopulation) populationFilter[Op.lte] = maxPopulation.toString();
+  console.log(populationFilter);
+  console.log(minPopulation, maxPopulation, limit, sortOrder);
   try {
-    /* const countries = await country.findAll({
-                                          include: [
-                                            {
-                                              model: population,
-                                              as: "populationData",
-                                              where: {
-                                                total_population: { [Op.get]: tp },
-                                              },
-                                            },
-                                          ],
-                                    
-                                    
-                                          order: [
-                                            [
-                                              { model: population, as: "populationData" },
-                                              "total_population",
-                                              sortOrder.toUpperCase(),
-                                            ],
-                                          ],
-                                          limit: parseInt(limit, 10),
-                                        });
-                                    
-                                        res.json(countries);
-                                    
-                                    
-                                            */
+    const countries = await country.findAll({
+      include: [
+        {
+          model: population,
+          as: "populationData",
+          where: {
+            total_population: populationFilter,
+          },
+        },
+      ],
 
-    const popuRes = await population.findAll({
+      order: [
+        [
+          { model: population, as: "populationData" },
+          "total_population",
+          sortOrder.toUpperCase(),
+        ],
+      ],
+      limit: parseInt(limit, 10),
+    });
+
+    res.json(countries);
+
+    /* const popuRes = await population.findAll({
       where: {
         total_population: {
           [Op.lte]: tp.toString(), // or another operator like Op.lte for less than
@@ -53,7 +50,7 @@ const getCountriesByPopulation = catchAsync(async (req, res, next) => {
       },
     });
 
-    res.json(popuRes);
+    res.json(popuRes); */
   } catch (error) {
     console.error("Error retrieving countries:", error);
     res.status(500).json({ error: "Faild to retrieve countries" });
